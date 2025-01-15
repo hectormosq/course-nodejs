bb2c027a78b66734985d886ddd8a35b6e9c6f4c4
Hector Mosquera <hectormosq@gmail.com>
2025-01-15 12:36:49 +0100

    Creating Simple response

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>')
+    res.write('<head><title>My First Page</title></head>')
+    res.write('<body>Hello from Node.js Server!</body>')
+    res.write('</html>')
+    res.end();
 });
 
 server.listen(3000);
