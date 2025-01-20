22da441d4947089a3eb6c0a7940b631e6cf34ca1
Hector Mosquera <hectormosq@gmail.com>
2025-01-20 16:34:28 +0100

    Installing template pug and implement

--- routes/shop.js
+++ routes/shop.js
@@ -7,7 +7,8 @@ const adminData = require('./admin')
 
 router.get("/", (req, res, next) => {
   console.log('shop.js', adminData.products)
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render('shop')
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
