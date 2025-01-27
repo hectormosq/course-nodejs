9869385cb248558cad3cada1fd891f0ca1fea06c
Hector Mosquera <hectormosq@gmail.com>
2025-01-27 20:42:19 +0100

    Adding custom validator

--- routes/auth.js
+++ routes/auth.js
@@ -8,7 +8,19 @@ const router = express.Router();
 router.get("/login", authController.getLogin);
 router.get("/signup", authController.getSignup);
 router.post("/login", authController.postLogin);
-router.post("/signup", check("email").isEmail().withMessage('Please enter a valid email'), authController.postSignup);
+router.post(
+  "/signup",
+  check("email")
+    .isEmail()
+    .withMessage("Please enter a valid email")
+    .custom((value, { req }) => {
+      if (value === "test@test.com") {
+        throw new Error("This email address is forbidden");
+      }
+      return true;
+    }),
+  authController.postSignup
+);
 router.post("/logout", authController.postLogout);
 
 module.exports = router;
