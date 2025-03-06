814d5d8587983ea4aada5e1e259266458ad7ee32
Hector Mosquera <hectormosq@gmail.com>
2025-03-06 17:59:52 +0100

    Removing credentials for db connection

--- util/connection.js
+++ util/connection.js
@@ -1,2 +1,10 @@
-export const connectionParam =
-  "mongodb+srv://hectormosq:PASSWORD@cluster0.r35mj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const user = process.env.MONGODB_USER;
+const password = process.env.MONGODB_PASSWORD;
+const cluster = process.env.MONGODB_CLUSTER;
+const db = process.env.MONGODB_DB;
+
+export const connectionParam = `mongodb+srv://${user}:${password}@${cluster}/${db}`;
